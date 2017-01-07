fd5f9d79ea5c6b69bb231ef523e90a863e424c25
Rediet Tilahun Desta <redtil10@gmail.com>
2017-01-07 18:01:28 -0500

    flickr image api implemented. searching and sending data in json to frontend

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 var mongo = require('mongodb');
 var assert = require('assert');
+var Flickr = require('flickrapi');
+
+var flickrOptions = {
+    api_key:'4aaeddd00e3be920ffb847186e27534e',
+    secret:'ba71a1135ae7f57d'
+}
+
 
 
 var url = 'mongodb://redtil:future8:12@ds145848.mlab.com:45848/emotivebackend-db';
@@ -12,7 +19,34 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/get-data', function(req, res, next) {
-
+    Flickr.tokenOnly(flickrOptions,function(error,flickr){
+        flickr.photos.search({text:"Brazil",
+        tag:"Brasil",
+        page:1,
+        per_page:10,
+        sort:"relevance"},function(error,results){
+           console.log(results);
+           var photos = results.photos.photo;
+           var urlsJson = {"urls":[]};
+           for(photo in photos){
+               var title = photos[photo].title;
+               var source = "flickr";
+               var url = "https://farm" + photos[photo].farm +
+                        ".staticflickr.com/" + photos[photo].server + "/" + photos[photo].id +
+                    "_" + photos[photo].secret+ ".jpg";
+               var photo = {
+                   title:title,
+                   url:url,
+                   source:source
+               }
+               urlsJson.urls.push(photo);
+           }
+           res.send(urlsJson);
+        });
+        if(error) {
+            console.log(error);
+        }
+    })
 });
 
 router.post('/insert', function(req, res, next) {
